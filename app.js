1152c3daae9871128721b006b73cc8221c4a9727
suzouj <suzuyaj288@gmail.com>
2023-09-15 13:22:59 +0500

    final

---
+++ app.js
@@ -0,0 +1,16 @@
+var UserName = prompt('Введите ваше имя');
+var UserAge = +prompt('Введите ваш возраст');
+
+console.log('Я ' + UserName + ' мне ' + UserAge + ' лет');
+
+
+var UserTest1 = +prompt('Пример 1: 6 + 3 = ?')
+console.log('6 + 3 = ' + (6 + 3) + ' Ваш ответ: ' + UserTest1);
+var UserTest2 = +prompt('Пример 2: 6 - 3 = ?')
+console.log('6 - 3 = ' + (6 - 3) + ' Ваш ответ: ' + UserTest2);
+var UserTest3 = +prompt('Пример 3: 6 * 3 = ?')
+console.log('6 * 3 = ' + 6 * 3 + ' Ваш ответ: ' + UserTest3);
+var UserTest4 = +prompt('Пример 4: 6 / 3 = ?')
+console.log('6 / 3 = ' + 6 / 3 + ' Ваш ответ: ' + UserTest4);
+var UserTest5 = +prompt('Пример 5: 6 % 3 = ?')
+console.log('6 % 3 = ' + 6 % 3 + ' Ваш ответ: ' + UserTest5);
